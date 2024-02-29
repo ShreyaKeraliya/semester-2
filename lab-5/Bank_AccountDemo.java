35ed6301030931e1ed6a2b90d4f66ab2bcdb02d0
ShreyaKeraliya <140882904+ShreyaKeraliya@users.noreply.github.com>
2024-02-29 12:15:49 +0530

    Delete lab-5 directory

--- lab-5/Bank_AccountDemo.java
+++
@@ -1,7 +0,0 @@
-public class Bank_AccountDemo {
-    public static void main(String[] args) {
-        Bank_Account a1= new Bank_Account();
-         a1.getAccountDetails();
-         a1.displayAccountDetails();
-    }
-}
--- lab-5/Circle.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Circle {
-     double radius;
-     void area(double r){
-        radius = r;
-        double area =Math.PI*r*r;
-        System.out.print(area);
-    } 
-}
