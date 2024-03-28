e843283f0d472f7a519631b479e45071c49d6113
ShreyaKeraliya <140882904+ShreyaKeraliya@users.noreply.github.com>
2024-03-28 22:59:50 +0530

    Add files via upload

---
+++ lab-10/CharacterCounter.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CharacterCounter {
+    public static void main(String[] args) {
+        String filePath = "input.txt"; // Replace this with the path to your file
+        int charCount = 0, wordCount = 0, lineCount = 0;
+
+        try {
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                lineCount++;
+                charCount += line.length();
+                wordCount += line.split("\\s+").length;
+            }
+
+            scanner.close();
+
+            System.out.println("Number of characters: " + charCount);
+            System.out.println("Number of words: " + wordCount);
+            System.out.println("Number of lines: " + lineCount);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filePath);
+            e.printStackTrace();
+        }
+    }
+}
